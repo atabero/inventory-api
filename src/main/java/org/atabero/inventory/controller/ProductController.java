eef6e2e501a41dff1ff62fff8735abae4a44d425
atabero <abtabero@proton.me>
2025-07-20 15:25:12 +0200

    Fix: Añadida anotación @PathVariable en getById y cambio endpoint de desactivar a PATCH
    - Corregido el método getById para incluir @PathVariable en el parámetro id.
    - Modificado endpoint de desactivar producto para usar PATCH en lugar de GET, siguiendo buenas prácticas REST.

---
+++ src/main/java/org/atabero/inventory/controller/ProductController.java
@@ -0,0 +1,58 @@
+package org.atabero.inventory.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.atabero.inventory.dto.product.CreateProductDTO;
+import org.atabero.inventory.dto.product.ProductResponseDTO;
+import org.atabero.inventory.dto.product.UpdateProductDTO;
+import org.atabero.inventory.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAll() {
+        return ResponseEntity.ok(
+                productService.getAll()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                productService.getById(id)
+        );
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> create(@RequestBody @Valid CreateProductDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                productService.create(dto)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> update(@PathVariable Long id, @RequestBody @Valid UpdateProductDTO dto) {
+        return ResponseEntity.ok(
+                productService.update(id, dto)
+        );
+    }
+
+    @PatchMapping("/{id}/deactivate")
+    public ResponseEntity<Void> deactivate(@PathVariable Long id) {
+        productService.deactivate(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
