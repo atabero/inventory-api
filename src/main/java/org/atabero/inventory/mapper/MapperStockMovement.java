4f77a09254f3b076cb6de09803571b20ff18f597
atabero <abtabero@proton.me>
2025-07-21 19:03:24 +0200

    se añade el mapper para StockMovement

--- src/main/java/org/atabero/inventory/mapper/MapperStockMovement.java
+++ src/main/java/org/atabero/inventory/mapper/MapperStockMovement.java
@@ -1,4 +1,21 @@
 package org.atabero.inventory.mapper;
 
+import org.atabero.inventory.dto.stockmovement.StockMovementResponseDTO;
+import org.atabero.inventory.model.StockMovement;
+
 public class MapperStockMovement {
+
+    private MapperStockMovement() {
+    }
+
+
+    public static StockMovementResponseDTO toResponse(StockMovement movement, Integer previousQuantity, Integer newQuantity) {
+        return new StockMovementResponseDTO(
+                movement.getId(), movement.getProduct().getId(), movement.getProduct().getName(),
+                previousQuantity, newQuantity,
+                movement.getQuantityChange(), movement.getMovementType(), movement.getOperationStatus(),
+                movement.getTimestamp(), movement.getNotes()
+        );
+    }
+
 }
